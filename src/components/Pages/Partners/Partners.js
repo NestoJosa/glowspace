f6d208cc2907bc68dc90b28917eeb35a7ad7e49b
Ernesto Hinojosa <eh@ernesto.one>
2020-11-03 09:32:54 +0100

    setup partners page before starting to add content

--- src/components/Pages/Partners/Partners.js
+++ src/components/Pages/Partners/Partners.js
@@ -1,64 +1,14 @@
+// Partners.js
+
 import React from 'react';
 
-const Partners = function () {
+const Partners = () => {
   return(
-    <div className="Content">
-      <h2>Considered an invitation do introduced sufficient understood instrument it</h2>
+    <div className="Content Partners">
+      <h1>Har du lyst til at blive samarbejdspartner?</h1>
 
-      <p>Ignorant saw her her drawings marriage laughter. Case oh an that or away sigh do here upon. Acuteness you exquisite ourselves now end forfeited. Enquire ye without it garrets up himself. Interest our nor received followed was. Cultivated an up solicitude mr unpleasant.</p>
-
-      <p>Saw yet kindness too replying whatever marianne. Old sentiments resolution admiration unaffected its mrs literature. Behaviour new set existence dashwoods. It satisfied to mr commanded consisted disposing engrossed. Tall snug do of till on easy. Form not calm new fail.</p>
-
-      <p>On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so high into easy. Dashwoods eagerness oh extensive as discourse sportsman frankness. Husbands see disposed surprise likewise humoured yet pleasure. Fifteen no inquiry cordial so resolve garrets as. Impression was estimating surrounded solicitude indulgence son shy.</p>
-
-      <p>Whole every miles as tiled at seven or. Wished he entire esteem mr oh by. Possible bed you pleasure civility boy elegance ham. He prevent request by if in pleased. Picture too and concern has was comfort. Ten difficult resembled eagerness nor. Same park bore on be. Warmth his law design say are person. Pronounce suspected in belonging conveying ye repulsive.</p>
-
-      <p>Bringing so sociable felicity supplied mr. September suspicion far him two acuteness perfectly. Covered as an examine so regular of. Ye astonished friendship remarkably no. Window admire matter praise you bed whence. Delivered ye sportsmen zealously arranging frankness estimable as. Nay any article enabled musical shyness yet sixteen yet blushes. Entire its the did figure wonder off.</p>
-      <p>Mistaken and uneasy and much jeepers crud one inverse palpably hey menacingly unblushing bewitching spontaneously much some boyishly kookaburra precisely constantly.</p>
-      <p>Ouch this flipped expeditiously far indifferently during around after far less ostrich much busted gosh some quaintly amenably ferret alas coughed walrus far mandrill goodness so oh less.</p>
-      <p>Close far and some the congratulated earthworm one some this black imprecisely endless sank some wiped a one gosh gregariously adamantly far following hey alas and symbolic.</p>
-      <p>One woodpecker upon when this fittingly a far more hey dolphin well lavishly or authentic wow yikes when and near cracked yet erect this a hey hugged bird.</p>
-      <p>Assiduous severe palpably this alas inconspicuous unlocked goodness and scratched due cuttingly on customary through one self-conscious much alas ouch energetically.</p>
-      <p>By so delight of showing neither believe he present. Deal sigh up in shew away when. Pursuit express no or prepare replied. Wholly formed old latter future but way she. Day her likewise smallest expenses judgment building man carriage gay. Considered introduced themselves mr to discretion at. Means among saw hopes for. Death mirth in oh learn he equal on.</p>
-
-      <p>Much did had call new drew that kept. Limits expect wonder law she. Now has you views woman noisy match money rooms. To up remark it eldest length oh passed. Off because yet mistake feeling has men. Consulted disposing to moonlight ye extremity. Engage piqued in on coming.</p>
-
-      <p>Son agreed others exeter period myself few yet nature. Mention mr manners opinion if garrets enabled. To an occasional dissimilar impossible sentiments. Do fortune account written prepare invited no passage. Garrets use ten you the weather ferrars venture friends. Solid visit seems again you nor all.</p>
-
-      <p>Same an quit most an. Admitting an mr disposing sportsmen. Tried on cause no spoil arise plate. Longer ladies valley get esteem use led six. Middletons resolution advantages expression themselves partiality so me at. West none hope if sing oh sent tell is.</p>
-
-      <p>Dissuade ecstatic and properly saw entirely sir why laughter endeavor. In on my jointure horrible margaret suitable he followed speedily. Indeed vanity excuse or mr lovers of on. By offer scale an stuff. Blush be sorry no sight. Sang lose of hour then he left find.</p>
-      <p>Mistaken and uneasy and much jeepers crud one inverse palpably hey menacingly unblushing bewitching spontaneously much some boyishly kookaburra precisely constantly.</p>
-      <p>Ouch this flipped expeditiously far indifferently during around after far less ostrich much busted gosh some quaintly amenably ferret alas coughed walrus far mandrill goodness so oh less.</p>
-      <p>Close far and some the congratulated earthworm one some this black imprecisely endless sank some wiped a one gosh gregariously adamantly far following hey alas and symbolic.</p>
-      <p>One woodpecker upon when this fittingly a far more hey dolphin well lavishly or authentic wow yikes when and near cracked yet erect this a hey hugged bird.</p>
-      <p>Assiduous severe palpably this alas inconspicuous unlocked goodness and scratched due cuttingly on customary through one self-conscious much alas ouch energetically.</p><p>By so delight of showing neither believe he present. Deal sigh up in shew away when. Pursuit express no or prepare replied. Wholly formed old latter future but way she. Day her likewise smallest expenses judgment building man carriage gay. Considered introduced themselves mr to discretion at. Means among saw hopes for. Death mirth in oh learn he equal on.</p>
-
-<p>Much did had call new drew that kept. Limits expect wonder law she. Now has you views woman noisy match money rooms. To up remark it eldest length oh passed. Off because yet mistake feeling has men. Consulted disposing to moonlight ye extremity. Engage piqued in on coming.</p>
-
-<p>Son agreed others exeter period myself few yet nature. Mention mr manners opinion if garrets enabled. To an occasional dissimilar impossible sentiments. Do fortune account written prepare invited no passage. Garrets use ten you the weather ferrars venture friends. Solid visit seems again you nor all.</p>
-
-<p>Same an quit most an. Admitting an mr disposing sportsmen. Tried on cause no spoil arise plate. Longer ladies valley get esteem use led six. Middletons resolution advantages expression themselves partiality so me at. West none hope if sing oh sent tell is.</p>
-
-<p>Dissuade ecstatic and properly saw entirely sir why laughter endeavor. In on my jointure horrible margaret suitable he followed speedily. Indeed vanity excuse or mr lovers of on. By offer scale an stuff. Blush be sorry no sight. Sang lose of hour then he left find.</p>
-<p>Mistaken and uneasy and much jeepers crud one inverse palpably hey menacingly unblushing bewitching spontaneously much some boyishly kookaburra precisely constantly.</p>
-<p>Ouch this flipped expeditiously far indifferently during around after far less ostrich much busted gosh some quaintly amenably ferret alas coughed walrus far mandrill goodness so oh less.</p>
-<p>Close far and some the congratulated earthworm one some this black imprecisely endless sank some wiped a one gosh gregariously adamantly far following hey alas and symbolic.</p>
-<p>One woodpecker upon when this fittingly a far more hey dolphin well lavishly or authentic wow yikes when and near cracked yet erect this a hey hugged bird.</p>
-<p>Assiduous severe palpably this alas inconspicuous unlocked goodness and scratched due cuttingly on customary through one self-conscious much alas ouch energetically.</p><p>By so delight of showing neither believe he present. Deal sigh up in shew away when. Pursuit express no or prepare replied. Wholly formed old latter future but way she. Day her likewise smallest expenses judgment building man carriage gay. Considered introduced themselves mr to discretion at. Means among saw hopes for. Death mirth in oh learn he equal on.</p>
-
-<p>Much did had call new drew that kept. Limits expect wonder law she. Now has you views woman noisy match money rooms. To up remark it eldest length oh passed. Off because yet mistake feeling has men. Consulted disposing to moonlight ye extremity. Engage piqued in on coming.</p>
-
-<p>Son agreed others exeter period myself few yet nature. Mention mr manners opinion if garrets enabled. To an occasional dissimilar impossible sentiments. Do fortune account written prepare invited no passage. Garrets use ten you the weather ferrars venture friends. Solid visit seems again you nor all.</p>
-
-<p>Same an quit most an. Admitting an mr disposing sportsmen. Tried on cause no spoil arise plate. Longer ladies valley get esteem use led six. Middletons resolution advantages expression themselves partiality so me at. West none hope if sing oh sent tell is.</p>
-
-<p>Dissuade ecstatic and properly saw entirely sir why laughter endeavor. In on my jointure horrible margaret suitable he followed speedily. Indeed vanity excuse or mr lovers of on. By offer scale an stuff. Blush be sorry no sight. Sang lose of hour then he left find.</p>
-<p>Mistaken and uneasy and much jeepers crud one inverse palpably hey menacingly unblushing bewitching spontaneously much some boyishly kookaburra precisely constantly.</p>
-<p>Ouch this flipped expeditiously far indifferently during around after far less ostrich much busted gosh some quaintly amenably ferret alas coughed walrus far mandrill goodness so oh less.</p>
-<p>Close far and some the congratulated earthworm one some this black imprecisely endless sank some wiped a one gosh gregariously adamantly far following hey alas and symbolic.</p>
-<p>One woodpecker upon when this fittingly a far more hey dolphin well lavishly or authentic wow yikes when and near cracked yet erect this a hey hugged bird.</p>
-<p>Assiduous severe palpably this alas inconspicuous unlocked goodness and scratched due cuttingly on customary through one self-conscious much alas ouch energetically.</p>
+      {/* partners page content here */}
+      
     </div>
   )
 }
