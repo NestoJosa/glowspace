3b49f292f52b129752a01670765300c908a4c622
Ernesto Hinojosa <eh@ernesto.one>
2020-11-14 10:35:25 +0100

    refactor the footer to use an outer wrapper, which has a black bg to fix issue on larger screens

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
-import React from 'react';
+/* Footer.js */
 
+import React from 'react';
 import LineWithDots from '../Line/Line';
 import LogoRound from '../LogoRound/LogoRound';
 import ContactForm from '../ContactForm/ContactForm';
@@ -10,31 +11,32 @@ import Legal from './Legal/Legal';
 
 const Footer = () => {
   return(
-    <div className="FooterWrapper FooterWrapper--twoRows FooterWrapper--oneRow">
-
-      <div className="FooterWrapper__item">
-        <LogoRound />
-      </div>
-      <div className="FooterWrapper__item">
-        <ContactForm />
-      </div>
-      <div className="FooterWrapper__item">
-        <LinksBlock data={links} />
-      </div>
-      <div className="FooterWrapper__item">
-        <LinksBlock data={aboutUs} />
-      </div>
-      <div className="FooterWrapper__item">
-        <AddressBlock />
-      </div>
-      <div className="FooterWrapper__item">
-        <SoMe />
-      </div>
-      <div className="FooterWrapper__item">
-        <LineWithDots />
-      </div>
-      <div className="FooterWrapper__item">
-        <Legal />
+    <div className="FooterWrapper">
+      <div className="Footer Footer--twoRows Footer--oneRow">
+        <div className="Footer__item">
+          <LogoRound />
+        </div>
+        <div className="Footer__item">
+          <ContactForm />
+        </div>
+        <div className="Footer__item">
+          <LinksBlock data={links} />
+        </div>
+        <div className="Footer__item">
+          <LinksBlock data={aboutUs} />
+        </div>
+        <div className="Footer__item">
+          <AddressBlock />
+        </div>
+        <div className="Footer__item">
+          <SoMe />
+        </div>
+        <div className="Footer__item">
+          <LineWithDots />
+        </div>
+        <div className="Footer__item">
+          <Legal />
+        </div>
       </div>
     </div>
   )
